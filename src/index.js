4075c23cc7b6e429b2b8fa38a5e7bff758f8a88f
ivan <ipkozyrin@gmail.com>
2016-12-05 19:50:46 +1000

    refactor remove Main.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import _ from 'lodash';
 
-import Main from './Main';
+import Config from './Config';
+import Log from './Log';
+import Form from './Form';
+import Storage from './Storage';
 
-const globalConfig = {};
+const globalConfig = {
+  debounceTime: 300,
+  unchangedValueSaving: false,
+  focusedFieldSaving: false,
+};
 
 export default {
   configure: function (config) {
     _.extend(globalConfig, config);
   },
-  newForm: function(config, schema) {
-    const newConfig = _.defaults(_.clone(config), globalConfig);
-    return new Main(newConfig, schema);
+  newForm: function(config) {
+    // const newConfig = _.defaults(_.clone(config), globalConfig);
+
+    const configInstance = new Config({});
+    const thisconfig = configInstance.get();
+    // TODO: не подключать так
+    const events = thisconfig.eventEmitter;
+    // TODO: не хранить silent - использовать глобальную опцию
+    const log = new Log({silent: thisconfig.silent});
+    const storage = new Storage();
+
+    return new Form(storage, events, log);
   },
 }
