e5900e287d0233c5d85aec00846eaa152d106dd9
ivan <ipkozyrin@gmail.com>
2016-12-09 08:39:45 +1000

    refactor

--- src/Form.js
+++ src/Form.js
@@ -21,37 +21,6 @@ export default class Form extends FormBase{
   }
 
   /**
-   * It set all the values to fields silently.
-   * It creates a field if it doesn't exist.
-   * @param newValues
-   */
-  setValues(newValues) {
-    this.$fieldsManager.updateValues(_.cloneDeep(newValues));
-  }
-
-  /**
-   * It set initial values for all the fields.
-   * It creates a field if it doesn't exist.
-   * It set value if it doesn't assign.
-   * @param initialState
-   */
-  setInitialValues(initialState) {
-    this.$fieldsManager.setInitialValues(initialState);
-  }
-
-  on(eventName, cb) {
-    this.$events.addListener(eventName, cb);
-  }
-
-  onChange(cb) {
-    this.$handlers.$onChangeCallback = cb;
-  }
-
-  onSave(cb) {
-    this.$handlers.$onSaveCallback = cb;
-  }
-
-  /**
    * It must be placed to <form> element on onSubmit attribute.
    */
   handleSubmit() {
@@ -69,7 +38,38 @@ export default class Form extends FormBase{
     this.$formState.setStateValue('submitting', false);
   }
 
+  on(eventName, cb) {
+    this.$events.addListener(eventName, cb);
+  }
+
+  onChange(cb) {
+    this.$handlers.$onChangeCallback = cb;
+  }
+
+  onSave(cb) {
+    this.$handlers.$onSaveCallback = cb;
+  }
+
   onSubmit(cb) {
     this._onSubmitCallback = cb;
+  }
+
+  /**
+   * It set all the values to fields silently.
+   * It creates a field if it doesn't exist.
+   * @param newValues
+   */
+  setValues(newValues) {
+    this.$fieldsManager.updateValues(_.cloneDeep(newValues));
+  }
+
+  /**
+   * It set initial values for all the fields.
+   * It creates a field if it doesn't exist.
+   * It set value if it doesn't assign.
+   * @param initialState
+   */
+  setInitialValues(initialState) {
+    this.$fieldsManager.setInitialValues(initialState);
   }
 }
