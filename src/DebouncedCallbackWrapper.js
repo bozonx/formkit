7ddd24ef6aabc62080bac4c489a460d14ca69dfb
ivan <ipkozyrin@gmail.com>
2017-05-31 20:26:21 +1000

    chore

--- src/DebouncedCallbackWrapper.js
+++ src/DebouncedCallbackWrapper.js
@@ -5,27 +5,25 @@ import _ from 'lodash';
  */
 export default class DebouncedCallbackWrapper {
   constructor() {
-    this._resolve = null;
-    this._reject = null;
-    this._promise = new Promise((resolve, reject) => {
-      this._resolve = resolve;
-      this._reject = reject;
+    this._mainResolve = null;
+    this._mainReject = null;
+    this._mainPromise = new Promise((resolve, reject) => {
+      this._mainResolve = resolve;
+      this._mainReject = reject;
     });
-    this._cb = null;
-    this._cbParams = [];
+    this._callback = null;
     this._cbPromise = null;
     this._started = false;
     this._pending = false;
   }
 
   getPromise() {
-    return this._promise;
+    return this._mainPromise;
   }
 
-  setCallback(cb, ...params) {
-    this._cb = cb;
-    this._cbParams = params;
-    // TODO: либо отклонять те что в процессе, либо отменять их результат и запускать заного
+  setCallback(cb, params) {
+    if (this._started) throw new Error(`The current callback is in progress, you can't set another one.`);
+    this._callback = { cb, params };
   }
 
   isPending() {
@@ -41,39 +39,31 @@ export default class DebouncedCallbackWrapper {
   }
 
   start() {
-    if (!this._cb) throw new Error(`There isn't a callback to run!`);
+    if (!this._callback) throw new Error(`There isn't a callback to run!`);
 
     this._started = true;
     this._pending = true;
 
-    this._cbPromise = this._cb(...this._cbParams);
-    if (!_.has(this._cbPromise, 'then')) {
+    this._cbPromise = this._callback.cb(...this._callback.params);
+    if (!this._isPromise(this._cbPromise)) {
       this._cbPromise = Promise.resolve();
     }
 
     this._cbPromise.then((data) => {
-      this._afterCbFulfilled();
+      this._pending = false;
+      this._mainResolve();
 
       return data;
     }).catch((err) => {
-      this._afterCbError();
+      this._pending = false;
+      this._mainReject();
 
       return Promise.reject(err);
     });
   }
 
-  _afterCbFulfilled() {
-    this._pending = false;
-    this._resolve();
-    // if (this._runAfterCbFulfill) {
-    //   this.exec(this._runAfterCbFulfill.cb, true, ...this._runAfterCbFulfill.params);
-    //   this._runAfterCbFulfill = undefined;
-    // }
-  }
-
-  _afterCbError() {
-    this._pending = false;
-    this._reject();
+  _isPromise(unnown) {
+    return _.isObject(unnown) && unnown.then;
   }
 
 }
