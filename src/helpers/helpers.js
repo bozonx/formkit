40ff403eb786ea10cddcaf0d3aa3bd3c49d01034
ivan <ipkozyrin@gmail.com>
2018-04-05 14:11:58 +1000

    fix

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -145,6 +145,10 @@ module.exports = {
     else if (rawValue === '') {
       return '';
     }
+    // it is for - 2. strings
+    else if (_.isString(rawValue) && rawValue.match(/^\d+\.$/)) {
+      return rawValue;
+    }
     else if (_.isBoolean(rawValue) || _.isPlainObject(rawValue) || _.isArray(rawValue)) {
       return rawValue;
     }
