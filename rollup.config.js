02cf3e02da102d1f71017c8041c22b3e15028fc9
Ivan <ipkozyrin@gmail.com>
2019-07-10 16:59:31 +1000

    change - remove webpack

--- rollup.config.js
+++ rollup.config.js
@@ -22,7 +22,7 @@ export default {
     // Allow json resolution
     json(),
     // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true, module: 'ESNext' }),
+    typescript({ useTsconfigDeclarationDir: true, tsconfigOverride: {compilerOptions: {module: 'ESNext'}}}),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
     // Allow node_modules resolution, so you can use 'external' to control
