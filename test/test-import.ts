bcc2284211ce8920772f8b6da05f7ad63bbb448d
Ivan <ipkozyrin@gmail.com>
2019-07-10 13:11:42 +1000

    fix build

---
+++ test/test-import.ts
@@ -0,0 +1,5 @@
+// Run this file - ./node_modules/.bin/ts-node ./test/test-import.ts
+
+import * as Formkit from '../dist/formkit.module.js';
+
+console.log(Object.keys(Formkit));
--- webpack.common.config.js
+++ webpack.common.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
 
+console.log(process.env.NODE_ENV)
+
 module.exports = {
   entry: './src/index',
   output: {
@@ -8,5 +10,17 @@ module.exports = {
     library: 'Formkit',
     sourceMapFilename: '[file].map',
   },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        //exclude: /node_modules/,
+      },
+    ],
+  },
   cache: false,
+  resolve: {
+    extensions: ['.js', '.json', '.ts'],
+  },
 };
